7762003d9c6fe1fe7e740ece5afb14b36468e73b
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-08-20 18:48:17 +0530

    joi validation

--- models/Validation.js
+++ models/Validation.js
@@ -3,5 +3,21 @@ const Joi = require('joi')
 const joiUserSchema =Joi.object({
     name:Joi.string().alphanum().min(3).max(30).required(),
     email:Joi.string().email().required(),
-    password:Joi.string()
+    password:Joi.string().required(),
+
 })
+
+const joiProductSchema =Joi.object({
+    id:Joi.string(),
+    name: Joi.string().required(),
+    description:Joi.string(),
+    price: Joi.number().positive(),
+    image: Joi.string(),
+    category:Joi.string(),
+})
+
+
+module.exports={
+    joiUserSchema,
+    joiProductSchema
+}
